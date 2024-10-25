fa3bf2e7b5663db8f949cb57cab427fac8664ec8
Pesha375 <evanpeshawar@gmail.com>
2024-10-25 12:12:10 +0300

    Add new changes to the code

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
--- Insert a single row into the customers table
-INSERT INTO customers (customer_name, email, address)
-VALUES ('Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+-- Insert a single row into the customers table, ignoring if customer_id 1 already exists
+INSERT IGNORE INTO customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
